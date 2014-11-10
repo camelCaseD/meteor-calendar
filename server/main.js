1a1d061053c8e59b3347e6ae8e3a0b68ffd1674c
Brandon B. <brandon@camelcased.com>
2014-11-10 08:59:53 -0800

    Update main.js
    Disable domain check on email upon login.

--- server/main.js
+++ server/main.js
@@ -1,10 +1,10 @@
-Accounts.validateNewUser(function (user) {
-  var email = user.services.google.email;
-  var domain = email.replace(/.*@/, "");
+//Accounts.validateNewUser(function (user) {
+//  var email = user.services.google.email;
+//  var domain = email.replace(/.*@/, "");
 
-  if (domain !== "sistersk-12.org") {
-    throw new Meteor.Error(403, "You must have an account from Sisters School District to login.");
-  } else {
-    return true;
-  }
-});
+//  if (domain !== "sistersk-12.org") {
+//    throw new Meteor.Error(403, "You must have an account from Sisters School District to login.");
+//  } else {
+//    return true;
+//  }
+//});
